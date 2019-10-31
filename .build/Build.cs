1a164bf8c41e26918d0c6a2b1a29471ff822a9b8
David Driscoll <david.driscoll@gmail.com>
2019-10-30 23:47:58 -0400

    Updated to nuke latest

--- .build/Build.cs
+++ .build/Build.cs
@@ -6,7 +6,7 @@ using Rocket.Surgery.Nuke;
 [CheckBuildProjectConfigurations]
 [UnsetVisualStudioEnvironmentVariables]
 [PackageIcon("https://raw.githubusercontent.com/RocketSurgeonsGuild/graphics/master/png/social-square-thrust-rounded.png")]
-class Build : DotNetCoreBuild
+class Solution : DotNetCoreBuild, IDotNetCoreBuild
 {
     /// <summary>
     /// Support plugins are available for:
@@ -16,7 +16,20 @@ class Build : DotNetCoreBuild
     ///   - Microsoft VSCode           https://nuke.build/vscode
     /// </summary>
 
-    public static int Main() => Execute<Build>(x => x.Default);
+    public static int Main() => Execute<Solution>(x => x.Default);
 
-    Target Default => _ => _.DependsOn(DotNetCore);
+    Target Default => _ => _
+        .DependsOn(Restore)
+        .DependsOn(Build)
+        .DependsOn(Test)
+        .DependsOn(Pack)
+        ;
+
+    public new Target Restore => _ => _.With(DotNetCoreBuild.Restore, this);
+
+    public new Target Build => _ => _.With(DotNetCoreBuild.Build, this);
+
+    public new Target Test => _ => _.With(DotNetCoreBuild.Test, this);
+
+    public new Target Pack => _ => _.With(DotNetCoreBuild.Pack, this);
 }
