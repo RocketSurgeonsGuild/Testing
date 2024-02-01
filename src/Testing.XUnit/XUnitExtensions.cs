af7ceb7843121b67943c4979acfe32e223ac73a1
David Driscoll <david.driscoll@gmail.com>
2024-02-01 13:03:05 -0500

    Added helper methods to compute an int of the given test, that can be used to give a semi-unique seed to tests in a test suite for greater variability (#1080)

--- src/Testing.XUnit/XUnitExtensions.cs
+++ src/Testing.XUnit/XUnitExtensions.cs
@@ -1,23 +1,4 @@
-using System.Reflection;
-using Xunit.Abstractions;
+using System.Runtime.CompilerServices;
+using Xunit;
 
-// ReSharper disable once CheckNamespace
-namespace Xunit;
-
-/// <summary>
-///     Helpful XUnit Extensions
-/// </summary>
-public static class XUnitExtensions
-{
-    /// <summary>
-    ///     Gets the test from the ITestOutputHelper
-    /// </summary>
-    /// <param name="output"></param>
-    /// <returns></returns>
-    public static ITest GetTest(this ITestOutputHelper output)
-    {
-        var type = output.GetType();
-        var testMember = type.GetField("test", BindingFlags.Instance | BindingFlags.NonPublic)!;
-        return (ITest)testMember.GetValue(output)!;
-    }
-}
+[assembly: TypeForwardedTo(typeof(XUnitExtensions))]
