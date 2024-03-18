9888127cc032c1b7096a2f7b08bc378f905a8c6c
David Driscoll <david.driscoll@gmail.com>
2024-03-17 22:42:33 -0400

    fixed failing tests, related to updated code.

--- test/Testing.Tests/ModuleInitializer.cs
+++ test/Testing.Tests/ModuleInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 using DiffEngine;
+using Microsoft.CodeAnalysis;
 using Rocket.Surgery.Extensions.Testing.SourceGenerators;
 
 namespace Rocket.Surgery.Extensions.Testing.Tests;
@@ -9,7 +10,7 @@ public static class ModuleInitializer
     [ModuleInitializer]
     public static void Init()
     {
-        VerifyGeneratorTextContext.Initialize(true);
+        VerifyGeneratorTextContext.Initialize(true, diagnosticSeverityFilter: DiagnosticSeverity.Warning);
 
         DiffRunner.Disabled = true;
     }
