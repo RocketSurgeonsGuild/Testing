b32fe16f48b0f3dce97d588b68d89c1dc130ac0a
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2024-03-25 23:30:52 -0500

    With without this

--- test/Testing.AutoFixtures.Tests/AutoFixtureGeneratorTests.GivenDeckSource_WhenGenerate_ThenGeneratedAutoFixture_619350127f939922#Deck.AutoFixture.g.verified.cs
+++ test/Testing.AutoFixtures.Tests/AutoFixtureGeneratorTests.GivenDeckSource_WhenGenerate_ThenGeneratedAutoFixture_619350127f939922#Deck.AutoFixture.g.verified.cs
@@ -8,7 +8,7 @@ namespace Goony.Goo.Goo.Tests
     internal sealed partial class DeckFixture : AutoFixtureBase
     {
         public static implicit operator Deck(DeckFixture fixture) => fixture.Build();
-        public DeckFixture WithEnumerable(System.Collections.Generic.IEnumerable<Goony.Goo.Goo.Card> cards) => this.With(ref _cards, cards);
+        public DeckFixture WithEnumerable(System.Collections.Generic.IEnumerable<Goony.Goo.Goo.Card> cards) => With(ref _cards, cards);
         private Deck Build() => new Deck(_cards);
         private System.Collections.Generic.IEnumerable<Goony.Goo.Goo.Card> _cards = Substitute.For<System.Collections.Generic.IEnumerable<Goony.Goo.Goo.Card>>();
     }
