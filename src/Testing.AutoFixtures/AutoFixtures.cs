eb7580bfd40fb19866f3887036a1f39b0b081f85
Rodney Littles II <6969701+RLittlesII@users.noreply.github.com>
2024-11-29 23:54:47 -0600

    bug: AutoFixtureAttribute not resolving due to code generation version (#1520)

--- src/Testing.AutoFixtures/AutoFixtures.cs
+++ src/Testing.AutoFixtures/AutoFixtures.cs
@@ -1,9 +1,12 @@
-using System.Diagnostics;
+using System.Reflection;
 
 namespace Rocket.Surgery.Extensions.Testing.AutoFixtures;
 
 internal static class AutoFixtures
 {
-    public static string Version { get; } = FileVersionInfo.GetVersionInfo(typeof(AutoFixtureGenerator).Assembly.Location).ProductVersion;
+    public static string Version { get; } =
+        typeof(AutoFixtureGenerator).Assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+     ?? typeof(AutoFixtureGenerator).Assembly.GetName().Version.ToString();
+
     public static string CodeGenerationAttribute { get; } = $@"[System.CodeDom.Compiler.GeneratedCode(""AutoFixtures"", ""{Version}"")]";
 }
