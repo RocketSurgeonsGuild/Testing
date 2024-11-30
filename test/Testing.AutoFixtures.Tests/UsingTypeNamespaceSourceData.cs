fa251e1a0a88dedc1eaded1cffb37b6d45f94880
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2024-11-29 20:37:18 -0600

    bug: fix source class namespace generation

---
+++ test/Testing.AutoFixtures.Tests/UsingTypeNamespaceSourceData.cs
@@ -0,0 +1,74 @@
+using FakeItEasy;
+using NSubstitute;
+using Rocket.Surgery.Extensions.Testing.SourceGenerators;
+
+namespace Rocket.Surgery.Extensions.Testing.AutoFixtures.Tests;
+
+public class UsingTypeNamespaceSourceData : AutoFixtureSourceData
+{
+    public static TheoryData<GeneratorTestContext> Data =>
+        new()
+        {
+            DefaultBuilder()
+               .AddReferences(typeof(Fake))
+               .AddSources(ClassSource, AttributedFixtureSource)
+               .Build(),
+            DefaultBuilder()
+               .AddReferences(typeof(Substitute))
+               .AddSources(ClassSource, AttributedFixtureSource)
+               .Build(),
+            DefaultBuilder()
+               .AddReferences(typeof(Fake))
+               .AddSources(AttributedSource)
+               .Build(),
+            DefaultBuilder()
+               .AddReferences(typeof(Substitute))
+               .AddSources(AttributedSource)
+               .Build(),
+        };
+
+
+    private const string ClassSource = @"
+namespace Application.Features.ViewModels
+{
+    public class ViewModel
+    {
+        public ViewModel(IThing thing)
+        {
+        }
+    }
+
+    public interface IThing
+    {
+    }
+}";
+
+    private const string AttributedSource = @"using System;
+using Rocket.Surgery.Extensions.Testing.AutoFixtures;
+
+namespace Application.Features.ViewModels
+{
+    [AutoFixture]
+    public class ViewModel
+    {
+        public ViewModel(IThing thing)
+        {
+        }
+    }
+
+    public interface IThing
+    {
+    }
+}";
+
+    private const string AttributedFixtureSource = @"using System;
+using Rocket.Surgery.Extensions.Testing.AutoFixtures;
+
+namespace Application.Tests.Features.ViewModels
+{
+    [AutoFixture(typeof(ViewModel)]
+    internal partial class ViewModelFixture
+    {
+    }
+}";
+}
