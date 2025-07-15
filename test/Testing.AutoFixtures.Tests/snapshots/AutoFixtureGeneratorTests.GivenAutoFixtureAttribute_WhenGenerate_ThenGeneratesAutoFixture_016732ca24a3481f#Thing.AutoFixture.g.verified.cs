eda9cb4046a9fc4e7f2e0db91fc9b1f6cfd786dd
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-07-15 10:11:07 -0500

    added analyzers to base tests

---
+++ test/Testing.AutoFixtures.Tests/snapshots/AutoFixtureGeneratorTests.GivenAutoFixtureAttribute_WhenGenerate_ThenGeneratesAutoFixture_016732ca24a3481f#Thing.AutoFixture.g.verified.cs
@@ -0,0 +1,20 @@
+﻿//HintName: Rocket.Surgery.Extensions.Testing.AutoFixtures/Rocket.Surgery.Extensions.Testing.AutoFixtures.AutoFixtureGenerator/Thing.AutoFixture.g.cs
+using System;
+using System.Collections.ObjectModel;
+using Goony.Goo.Goo;
+using Rocket.Surgery.Extensions.Testing.AutoFixtures;
+
+namespace Goony.Goo.Goo.Tests
+{
+    internal sealed partial class ThingFixture : AutoFixtureBase<ThingFixture>
+    {
+        public static implicit operator Thing(ThingFixture fixture) => fixture.Build();
+        public ThingFixture WithThingOne(System.Int32 thingOne) => With(ref _thingOne, thingOne);
+        public ThingFixture WithThingTwo(Goony.Goo.Goo.Numbered thingTwo) => With(ref _thingTwo, thingTwo);
+        public ThingFixture WithThingThree(Goony.Goo.Goo.Colored thingThree) => With(ref _thingThree, thingThree);
+        private Thing Build() => new Thing(_thingOne, _thingTwo, _thingThree);
+        private System.Int32 _thingOne = default;
+        private Goony.Goo.Goo.Numbered _thingTwo = default;
+        private Goony.Goo.Goo.Colored _thingThree = default;
+    }
+}
---
+++ test/Testing.AutoFixtures.Tests/snapshots/AutoFixtureGeneratorTests.GivenAutoFixtureAttribute_WhenGenerate_ThenGeneratesAutoFixture_45bedcfca689fdba#LoadThings.QueryHandler.AutoFixture.g.verified.cs
@@ -0,0 +1,16 @@
+﻿//HintName: Rocket.Surgery.Extensions.Testing.AutoFixtures/Rocket.Surgery.Extensions.Testing.AutoFixtures.AutoFixtureGenerator/LoadThings.QueryHandler.AutoFixture.g.cs
+using System.Collections.ObjectModel;
+using FakeItEasy;
+using Goony.Goo.Goo;
+using Rocket.Surgery.Extensions.Testing.AutoFixtures;
+
+namespace Goony.Goo.Goo
+{
+    internal sealed partial class LoadThingsQueryHandlerFixture : AutoFixtureBase<LoadThingsQueryHandlerFixture>
+    {
+        public static implicit operator LoadThings.QueryHandler(LoadThingsQueryHandlerFixture fixture) => fixture.Build();
+        public LoadThingsQueryHandlerFixture WithThing(Goony.Goo.Goo.IThing thing) => With(ref _thing, thing);
+        private LoadThings.QueryHandler Build() => new LoadThings.QueryHandler(_thing);
+        private Goony.Goo.Goo.IThing _thing = A.Fake<Goony.Goo.Goo.IThing>();
+    }
+}
