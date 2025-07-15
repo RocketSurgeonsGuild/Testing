2a2ee3ba0d78f0267373f15f6e09c7b5270f5df9
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2025-07-15 08:43:35 -0500

    create descriptors and analyzers

--- src/Testing.AutoFixtures/Diagnostics/AutoFixture0001.cs
+++ src/Testing.AutoFixtures/Diagnostics/AutoFixture0001.cs
@@ -1,6 +1,58 @@
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
 namespace Rocket.Surgery.Extensions.Testing.AutoFixtures.Diagnostics;
 
-public class AutoFixture0001
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class AutoFixture0001 : DiagnosticAnalyzer
 {
-    
+    /// <summary>
+    ///     Diagnostic for unsupported classes with no constructors.
+    /// </summary>
+    public static DiagnosticDescriptor Descriptor = new(
+        nameof(AutoFixture0001),
+        "classes without constructors are currently not supported.",
+        "",
+        "Support",
+        DiagnosticSeverity.Info,
+        true
+    );
+
+    /// <inheritdoc />
+    public override void Initialize(AnalysisContext analysisContext)
+    {
+        analysisContext.EnableConcurrentExecution();
+        analysisContext.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+        analysisContext.RegisterSyntaxNodeAction(
+            action: context =>
+                    {
+                        var classDeclaration = (ClassDeclarationSyntax)context.Node;
+                        var hasAttribute =
+                            classDeclaration.AttributeLists.Any(listSyntax => listSyntax.Attributes.Any(syntax => syntax.IsAutoFixtureAttribute()));
+
+                        var constructors = classDeclaration.Members.OfType<ConstructorDeclarationSyntax>().ToImmutableList();
+
+                        if (!hasAttribute)
+                        {
+                            return;
+                        }
+
+                        if (constructors.All(constructorDeclarationSyntax =>
+                                                 constructorDeclarationSyntax.ParameterList.Parameters.All(parameterSyntax =>
+                                                     parameterSyntax.AttributeLists.Count == 0
+                                                 )
+                            ))
+                        {
+                            context.ReportDiagnostic(Diagnostic.Create(Descriptor, classDeclaration.GetLocation()));
+                        }
+                    },
+            syntaxKinds: SyntaxKind.ClassDeclaration
+        );
+    }
+
+    /// <inheritdoc />
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = [Descriptor];
 }
--- test/Testing.AutoFixtures.Tests/Diagnostics/AutoFixture0001Tests.cs
+++ test/Testing.AutoFixtures.Tests/Diagnostics/AutoFixture0001Tests.cs
@@ -1,6 +1,30 @@
+using Rocket.Surgery.Extensions.Testing.AutoFixtures.Diagnostics;
+using Rocket.Surgery.Extensions.Testing.SourceGenerators;
+
 namespace Rocket.Surgery.Extensions.Testing.AutoFixtures.Tests.Diagnostics;
 
 public class AutoFixture0001Tests
 {
-    
+    [Theory]
+    [MemberData(nameof(GenerateMultipleFixturesData.Data), MemberType = typeof(GenerateMultipleFixturesData))]
+    public async Task GivenNoConstructor_WhenGenerate_ThenReportsDiagnostic(GeneratorTestContext context)
+    {
+        // Given, When
+        var result = await context.GenerateAsync();
+
+        // Then
+        result
+           .Results
+           .ShouldContain(pair => pair.Value.Diagnostics.All(diagnostic => diagnostic.Id == AutoFixture0001.Descriptor.Id));
+    }
+
+    [Fact]
+    public async Task GivenDiagnosticReported_WhenGenerate_ThenGeneratesOtherFixtures()
+    {
+        // Given
+
+        // When
+
+        // Then
+    }
 }
